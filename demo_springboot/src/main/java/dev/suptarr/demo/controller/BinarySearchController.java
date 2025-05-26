93adf210d0557e9514041e0b14a120d0da84b810
supakrit.c <supakrit.c@arise.tech>
2025-05-26 12:04:06 +0700

    feat: add post to se list

--- demo_springboot/src/main/java/dev/suptarr/demo/controller/BinarySearchController.java
+++ demo_springboot/src/main/java/dev/suptarr/demo/controller/BinarySearchController.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import dev.suptarr.demo.model.BinarySearch;
@@ -11,12 +13,19 @@ import dev.suptarr.demo.model.exception.ValueNotFoundException;
 
 @RestController
 public class BinarySearchController {
-    List<Integer> sortedList = List.of(1, 3, 4, 6, 8, 9, 11);
+    List<Integer> sortedList = List.of();
     BinarySearch binarySearch = new BinarySearch(sortedList);
 
     @GetMapping("/binary-search/{number}")
     public int searchNumber(@PathVariable("number") String number) throws ValueNotFoundException {
         int num = Integer.parseInt(number);
         return binarySearch.indexOf(num);
+    }
+
+    @PostMapping("/binary-search")
+    public String setSortedList(@RequestBody List<Integer> newList) {
+        this.sortedList = newList;
+        this.binarySearch = new BinarySearch(this.sortedList);
+        return "List updated successfully. New list: " + this.sortedList.toString();
     }
 }
