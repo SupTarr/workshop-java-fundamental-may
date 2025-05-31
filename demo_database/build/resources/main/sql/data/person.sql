255b401409c5b526b26c8d1047add29a5cf72e7e
supakrit.c <supakrit.c@arise.tech>
2025-05-31 23:05:28 +0700

    clear git cache

--- demo_database/build/resources/main/sql/data/person.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO person (person_id, name, age)
-VALUES ('P001', 'Laumcing', 30) ON CONFLICT (person_id) DO NOTHING;
-
-INSERT INTO person (person_id, name, age)
-VALUES ('P002', 'Zothanmawia', 28) ON CONFLICT (person_id) DO NOTHING;
